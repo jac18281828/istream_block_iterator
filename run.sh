44b7610fc52fa5f99e6e00f1b123e6f9db286dae
John Cairns <john@2ad.com>
2021-10-28 11:10:18 -0500

    replace version once each day

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=$(date +%s)
+VERSION=$(date +%m%d%y)
 
 docker build . -t istream_block_iterator:${VERSION} && \
 	docker run --rm -i -t istream_block_iterator:${VERSION}
